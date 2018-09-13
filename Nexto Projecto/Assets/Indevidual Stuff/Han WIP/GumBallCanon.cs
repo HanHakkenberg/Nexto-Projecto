011d30e815011f0e6bf66ee91d3d1cccaf3a811a
Captain NeckBeard <han.hakkenberg@gmail.com>
2018-09-13 13:56:36 +0200

    Cannon

---
+++ Nexto Projecto/Assets/Indevidual Stuff/Han WIP/GumBallCanon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GumBallCanon : MonoBehaviour {
+    [SerializeField] string gumBallPool;
+    [SerializeField] Transform gumBallSpawnPos;
+    [SerializeField] float gumballSpawnRate;
+
+    void Update() {
+        GumballCheck();
+    }
+
+    float currentSpawnTime;
+    void GumballCheck() {
+        currentSpawnTime -= Time.deltaTime;
+
+        if(currentSpawnTime <= 0) {
+            currentSpawnTime = gumballSpawnRate;
+            ObjectPooler.instance.GetFromPool(gumBallPool, gumBallSpawnPos.position, gumBallSpawnPos.rotation);
+        }
+    }
+}
---
+++ Nexto Projecto/Assets/Indevidual Stuff/Han WIP/Gumball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Gumball : MonoBehaviour {
+    [SerializeField] float gumballSpeed;
+
+    void Update() {
+        transform.Translate(transform.right * Time.deltaTime * gumballSpeed);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("GumballInteraction")) {
+            transform.localEulerAngles = collision.contacts[0].normal * 90;
+            Debug.Log(transform.localEulerAngles);
+        }
+    }
+
+    public void AddToPool() {
+        ObjectPooler.instance.AddToPool("Gumball", gameObject);
+    }
+}
