83f58cb3aba41ee3666811368d02f303d91f4608
Rief Haalboom <superrief@hotmail.com>
2018-09-17 16:45:23 +0200

    Simpele UI, bezig met UnityEvents

---
+++ Nexto Projecto/Assets/Indevidual Stuff/Rief WIP/Scripts/ScriptableObjects WIP/GameEventManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventManager : MonoBehaviour {
+
+	public UIEvent theEvent;
+	public UnityEvent myEvents;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		if(Input.GetButton("Cancel") && myEvents != null)
+		{
+			myEvents.Invoke();
+		}
+	}
+	void Ping()
+	{
+		Debug.Log("Ping");
+	}
+}
---
+++ Nexto Projecto/Assets/Indevidual Stuff/Rief WIP/Scripts/ScriptableObjects WIP/UIEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class UIEvent : ScriptableObject {
+
+
+	
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
