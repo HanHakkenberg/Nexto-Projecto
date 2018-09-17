0dc6c514994417c3484c9be8d863451747e2af41
Captain NeckBeard <han.hakkenberg@gmail.com>
2018-09-17 17:08:48 +0200

    turning wheal

--- Nexto Projecto/Assets/Indevidual Stuff/Han WIP/TurningWheal.cs
+++ Nexto Projecto/Assets/Indevidual Stuff/Han WIP/TurningWheal.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurningWheal : MonoBehaviour {
+    [SerializeField] float speed;
+    [SerializeField] int minRot, maxRot;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update() {
+        if(Input.GetButton("Fire1") && transform.rotation.y >= minRot) {
+            transform.Rotate(new Vector3(0,speed * Time.deltaTime));
+        }
+        else if(Input.GetButton("Fire2") && transform.rotation.y <= maxRot) {
+            transform.Rotate(new Vector3(0,-speed * Time.deltaTime));
+        }
+    }
 }
