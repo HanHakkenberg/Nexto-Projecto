5cbd679d4141554f983e9aa59525ff6f095e0fea
Rief Haalboom <superrief@hotmail.com>
2018-09-25 13:39:54 +0200

    Volume options werken

--- Nexto Projecto/Assets/Indevidual Stuff/Rief WIP/Scripts/OptionManager.cs
+++ Nexto Projecto/Assets/Indevidual Stuff/Rief WIP/Scripts/OptionManager.cs
@@ -1,67 +1,129 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour {
 
-	public List<AudioSource> master;
+[Header("Lists")]
+	public List <AudioSource> music;
 	public List<AudioSource> effects;
 	public List<AudioSource> environmental;
+	
+[Header("Sliders")]
+	public Slider musicSlider;
+	public Slider effectSlider;
+	public Slider environmentSlider;
+	List<Slider> allSliders = new List<Slider>();
+
+[Header("Booleans")]
+	public bool subtitles = true;
+	public bool cutscenes = true;
 	bool masterOn = true;
+	bool musicOn = true;
 	bool effectsOn = true;
 	bool envirOn = true;
 
 	void Start () 
 	{
-		master.AddRange(effects);
-		master.AddRange(environmental);
+		allSliders.Add(musicSlider);
+		allSliders.Add(effectSlider);
+		allSliders.Add(environmentSlider);
 	}
 
-	void Update () 
-	{
-	}
-
-	public void masterClick()
+	public void MasterClick()
 	{
 		masterOn = !masterOn;
 
 	}
-	public void effectsClick()
+	public void MusicClick()
+	{
+		musicOn = !musicOn;
+	}
+	public void EffectsClick()
 	{
 		effectsOn = !effectsOn;
 	}
-	public void environmentalClick()
+	public void EnvironmentalClick()
 	{
 		envirOn = !envirOn;
 	}
-
-	public void applyClick()
+	public void SubtitleClick()
 	{
-		for(int i = 0; i < master.Count; i++)
+		subtitles = !subtitles;
+	}
+	public void CutsceneClick()
+	{
+		cutscenes = !cutscenes;
+	}
+
+	public void MasterVolume(Slider masterSlider)
+	{
+		for(int i = 0; i < allSliders.Count; i++)
 		{
-			if(masterOn){
-				master[i].GetComponent<AudioSource>().mute = false;
+			allSliders[i].value = masterSlider.value;
+		}
+	}
+
+	public void ApplyClick()
+	{
+		//SOUND RELATED
+		for(int o = 0; o < music.Count; o++)
+		{
+			music[o].GetComponent<AudioSource>().volume = musicSlider.GetComponent<Slider>().value;
+
+			if(musicOn && masterOn)
+			{
+				music[o].GetComponent<AudioSource>().mute = false;
 			}
 			else
 			{
-				master[i].GetComponent<AudioSource>().mute = true;
+				music[o].GetComponent<AudioSource>().mute = true;
 			}
 		}
-		for(int j = 0; j < environmental.Count; j++)
+
+		for(int k = 0; k < effects.Count; k++)
 		{
-			if(envirOn)
+			effects[k].GetComponent<AudioSource>().volume = effectSlider.GetComponent<Slider>().value;
+
+			if(effectsOn && masterOn)
 			{
-				if(environmental[j].tag == "MusicEnvironment"){
-				environmental[j].GetComponent<AudioSource>().mute = false;
-				}
+				effects[k].GetComponent<AudioSource>().mute = false;
 			}
 			else
 			{
-				if(!masterOn)
-				{
-					environmental[j].GetComponent<AudioSource>().mute = true;
-				}
+				effects[k].GetComponent<AudioSource>().mute = true;
 			}
+		}
+
+		for(int j = 0; j < environmental.Count; j++)
+		{
+			environmental[j].GetComponent<AudioSource>().volume = environmentSlider.GetComponent<Slider>().value;
+
+			if(envirOn && masterOn)
+			{
+				environmental[j].GetComponent<AudioSource>().mute = false;
+			}
+			else
+			{
+				environmental[j].GetComponent<AudioSource>().mute = true;
+			}
+		}
+
+		//SUBTITLE RELATED
+		if(subtitles)
+		{
+			//zodra in game UI klaar is kan ik hier mee verder
+		}
+		else
+		{
+			//zodra in game UI klaar is kan ik hier mee verder
+		}
+
+		//CUTSCENES RELATED
+		if(cutscenes)
+		{
+			//Weet nog niet hoe de cutscenes gedaan worden, als ik dat weet kan ik hier mee verder
 		}
 	}
 }
