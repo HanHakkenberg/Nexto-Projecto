9cf1be7967e67716b997c4442c5605175ad88ccd
JoppeStijfDeltion <jstijf99@gmail.com>
2018-10-05 10:12:51 +0200

    Pebbles

---
+++ Nexto Projecto/Assets/Individual work/Tony WIP/Script/Quest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour {
+	
+	public DialogueContainer npcInvolved;
+	public bool completed = false;
+
+	public void ThisQuest() {
+
+	}
+}
