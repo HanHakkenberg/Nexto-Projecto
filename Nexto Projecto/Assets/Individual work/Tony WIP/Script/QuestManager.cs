3785312b9a502c47c0c7a1fbd3a071ea483b67c2
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2018-10-05 09:05:16 +0200

    Questmanager

---
+++ Nexto Projecto/Assets/Individual work/Tony WIP/Script/QuestManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+
+	public List<Quest> inactive = new List<Quest>();
+	public List<Quest> active = new List<Quest>();
+	public List<Quest> finished = new List<Quest>();
+
+	public static QuestManager questManager;
+
+	private void Awake() {
+		if(questManager == null)
+		questManager = this;
+		else
+		Destroy(this);
+	}
+
+	public void QuestHandler() {
+
+	}
+}
