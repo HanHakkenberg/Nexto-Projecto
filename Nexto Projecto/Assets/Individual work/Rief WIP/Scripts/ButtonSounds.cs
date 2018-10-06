c87996e6ae904618118a5291ab30d1fdff03f327
Rief Haalboom <superrief@hotmail.com>
2018-10-06 16:54:41 +0200

    temp UI sounds added

---
+++ Nexto Projecto/Assets/Individual work/Rief WIP/Scripts/ButtonSounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour {
+
+	public AudioSource soundSource;
+	public AudioClip hoverSound;
+	public AudioClip clickSound;
+
+	public void OnHover()
+	{
+		soundSource.PlayOneShot(hoverSound);
+	}
+	public void OnClick()
+	{
+		soundSource.PlayOneShot(clickSound);
+	}
+}
