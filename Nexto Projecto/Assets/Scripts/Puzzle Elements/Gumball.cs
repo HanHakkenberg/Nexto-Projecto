dc50b93351308ebf05e025a7c325058478d2e4b5
han hakkenberg <han.hakkenberg@gmail.com>
2018-10-10 16:02:00 +0200

    Scene

--- Nexto Projecto/Assets/Scripts/Puzzle Elements/Gumball.cs
+++ Nexto Projecto/Assets/Scripts/Puzzle Elements/Gumball.cs
@@ -6,6 +6,7 @@ public class Gumball : MonoBehaviour {
     [SerializeField] float lifeSpenInSec;
     [SerializeField] UnityEvent onDestroy;
     [SerializeField] UnityEvent onStart;
+    [SerializeField] AudioSource myAudio;
     float currentLifeSpen;
 
     private void Start() {
@@ -30,6 +31,7 @@ public class Gumball : MonoBehaviour {
             Vector3 v = Vector3.Reflect(transform.forward, collision.contacts[0].normal);
             float newYRot = 90 - Mathf.Atan2(v.z, v.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, newYRot, 0);
+            myAudio.Play();
         }
         else if(!collision.gameObject.CompareTag("Projectile")) {
             onDestroy.Invoke();
