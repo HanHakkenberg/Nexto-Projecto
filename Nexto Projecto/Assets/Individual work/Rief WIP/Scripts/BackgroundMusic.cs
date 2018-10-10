933bc52272de9a3c17bffe66a6bf0acf3a9fc48a
Rief Haalboom <superrieff@hotmail.com>
2018-10-10 14:47:57 +0200

    donut flying puzzle WIP & background music change

---
+++ Nexto Projecto/Assets/Individual work/Rief WIP/Scripts/BackgroundMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    public AudioSource bgmSource;
+    public List<AudioClip> bgm = new List<AudioClip>();
+    public int currClip;
+    public bool fading;
+    public float fadeTime;
+
+	void Update()
+	{
+		if(fading)
+		{
+            bgmSource.volume -= fadeTime * Time.deltaTime;
+        }
+        else
+		{
+            bgmSource.volume += fadeTime * Time.deltaTime;
+        }
+        BGMUpdate();
+	}
+
+    public void BGMUpdate()
+	{
+        if (bgmSource.volume <= 0)
+        {
+            bgmSource.clip = bgm[currClip];
+            bgmSource.Play();
+			fading = false;
+        }
+    }
+}
---
+++ Nexto Projecto/Assets/Individual work/Rief WIP/Scripts/DonutPuzzle.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DonutPuzzle : MonoBehaviour {
+    
+    public TextMeshProUGUI timeText;
+    public int time;
+    public bool finished;
+    bool failed;
+    public bool puzzleActive = true;
+    public GameObject manager;
+
+    void Start () 
+    {
+        StartPuzzle();
+        //ToStart();
+    }
+    void ToStart()
+    {
+        StartCoroutine(Countdown());
+    }
+	
+	void Update () 
+    {
+        Finish();
+        StartTimer();
+    }
+
+    public IEnumerator Countdown()
+    {
+        time -= 1;
+        timeText.text = time.ToString();
+        timeText.GetComponent<Animator>().SetTrigger("TimeDown");
+        yield return new WaitForSeconds(1);
+        if (!finished)
+        {
+            ToStart();
+        }
+    }
+    void StartTimer()
+    {
+        if(Input.GetButtonDown("SwitchKey") && puzzleActive)
+        {
+            ToStart();
+        }
+    }
+
+    public void StartPuzzle()
+    {
+        timeText.text = "Press K to start flying!";
+        puzzleActive = true;
+    }
+
+    public void Finish()
+    {
+        if(finished)
+        {
+            timeText.text = "Press K to fly back!";
+            puzzleActive = false;
+        }
+    }
+    public void Failed()
+    {
+        if(time <= 0)
+        {
+            failed = true;
+        }
+    }
+}
