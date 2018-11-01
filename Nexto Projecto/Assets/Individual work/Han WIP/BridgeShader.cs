96d244a60c15ee350134d4b713f24ff37d8fd47d
han hakkenberg <han.hakkenberg@gmail.com>
2018-11-01 10:44:42 +0100

    Scene puzzle Update Shader Bridge Added

---
+++ Nexto Projecto/Assets/Individual work/Han WIP/BridgeShader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeShader : MonoBehaviour {
+    [SerializeField] Renderer rend;
+    [SerializeField] float amount;
+    [SerializeField] float Addspeed;
+
+    public void StartShader() {
+        StartCoroutine(Shader());
+    }
+
+    IEnumerator Shader() {
+        while(0 <= amount) {
+            amount -= Addspeed * Time.deltaTime;
+            rend.material.SetFloat("_SliceAmount", amount);
+            yield return null;
+        }
+    }
+}
