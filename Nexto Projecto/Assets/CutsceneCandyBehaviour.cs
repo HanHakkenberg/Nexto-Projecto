0313cd177fe8de7287d38991fb0ca4074d15ce4b
GuyvandermeulenDeltion <guy_meulen@live.nl>
2018-11-02 20:16:04 +0100

    Updated quest

---
+++ Nexto Projecto/Assets/CutsceneCandyBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneCandyBehaviour : MonoBehaviour {
+
+    public float activationTime = 4;
+    public Animator anim;
+
+    public void StartAnimation() {
+        StartCoroutine(Animate());
+    }
+
+    public IEnumerator Animate() {
+        yield return new WaitForSeconds(activationTime);
+        anim.enabled = true;
+    }
+} 
---
+++ Nexto Projecto/Assets/Individual work/Coen WIP/Scripts/FerrisWheel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FerrisWheel : MonoBehaviour {
+
+    public float speedIncreasePS = 1.2f;
+    public float minSpeed = 1;
+    public float maxSpeed = 8;
+
+    private Animator anim;
+    private float speed;
+
+    bool reachedTopSpeed = false;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update () {
+        Rotate();
+	}
+
+    void Rotate() {
+        if(anim.enabled)
+            if (reachedTopSpeed == false && GameManager.gameManager.gameTimeout == true) {
+            speed = Mathf.Lerp(speed, maxSpeed, speedIncreasePS * Time.deltaTime);
+            if(Mathf.Approximately(speed, maxSpeed)) { 
+             reachedTopSpeed = true;
+             }
+            } else {
+                speed = Mathf.Lerp(speed, minSpeed, speedIncreasePS * Time.deltaTime);
+            }
+
+        anim.SetFloat("Speed", speed);
+    }
+}
