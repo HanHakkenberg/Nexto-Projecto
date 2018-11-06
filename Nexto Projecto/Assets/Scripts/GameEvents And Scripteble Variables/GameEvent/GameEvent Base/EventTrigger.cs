cfac13c99d20354a8d484e0772e2ddc4f3bce8c0
han hakkenberg <han.hakkenberg@gmail.com>
2018-11-06 22:20:28 +0100

    Island 4 to 5 puzzle

--- Nexto Projecto/Assets/Scripts/GameEvents And Scripteble Variables/GameEvent/GameEvent Base/EventTrigger.cs
+++ Nexto Projecto/Assets/Scripts/GameEvents And Scripteble Variables/GameEvent/GameEvent Base/EventTrigger.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class EventTrigger : MonoBehaviour {
 	[SerializeField] GameEvent myEvent;
+    [SerializeField] UnityEvent unityEvents;
 
 	public void RaiseEvent() {
 		myEvent.Raise();
+        unityEvents.Invoke();
 	}
 }
