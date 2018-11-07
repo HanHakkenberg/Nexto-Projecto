84f6cea48d50b0db20313b8fcb1b9bafefdae0a5
han hakkenberg <han.hakkenberg@gmail.com>
2018-11-07 15:36:15 +0100

    Bug Fix First Animation(Not INplomented Yet)

--- Nexto Projecto/Assets/Scripts/GameEvents And Scripteble Variables/GameEvent/GameEvent Base/EventTrigger.cs
+++ Nexto Projecto/Assets/Scripts/GameEvents And Scripteble Variables/GameEvent/GameEvent Base/EventTrigger.cs
@@ -2,11 +2,13 @@
 using UnityEngine.Events;
 
 public class EventTrigger : MonoBehaviour {
-	[SerializeField] GameEvent myEvent;
+    [SerializeField] GameEvent myEvent;
     [SerializeField] UnityEvent unityEvents;
 
-	public void RaiseEvent() {
-		myEvent.Raise();
+    public void RaiseEvent() {
+        if(myEvent != null) {
+            myEvent.Raise();
+        }
         unityEvents.Invoke();
-	}
+    }
 }
