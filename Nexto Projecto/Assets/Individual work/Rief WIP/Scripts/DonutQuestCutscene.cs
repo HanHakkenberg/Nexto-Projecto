ac1b6774213c1813589915414cdc5c122be07452
Rief Haalboom <superrieff@hotmail.com>
2018-11-08 10:46:34 +0100

    cosmetics done, donut puzzle implemented, ui sprites implemented

---
+++ Nexto Projecto/Assets/Individual work/Rief WIP/Scripts/DonutQuestCutscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonutQuestCutscene : MonoBehaviour {
+
+    bool hasPlayed;
+
+    void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider _P)
+    {
+        if (_P.tag == "Player")
+        {
+            if (!hasPlayed)
+            {
+                CutsceneManager.cutsceneManager.LoadStartCutscene(11);
+                hasPlayed = true;
+            }
+        }
+    }
+}
