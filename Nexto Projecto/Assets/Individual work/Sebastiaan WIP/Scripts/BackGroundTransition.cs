8f828b1d85afd961fb5aa2ab925aa95aa7cf6efa
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2018-11-08 09:02:04 +0100

    rief is een ongeduldige fag reee

---
+++ Nexto Projecto/Assets/Individual work/Sebastiaan WIP/Scripts/BackGroundTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundTransition : MonoBehaviour {
+
+    public Color skyboxColor;
+    public Material skyboxMat;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        skyboxMat.color = skyboxColor;
+    }
+}
+
